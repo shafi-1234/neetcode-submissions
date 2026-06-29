6fb4665b0205dd7475ae194eaac4263610c89513
Mahammad shafi <mahammadshafishaik444@gmail.com>
2026-06-29 17:50:08 +0530

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        int n=s.length();
+        for(int i=0;i<n;i++){
+            char ch=s.charAt(i);
+            if(ch=='('){
+                stack.push(')');
+            }else if(ch=='{'){
+                stack.push('}');
+            }else if(ch=='['){
+                stack.push(']');
+            }else if(stack.isEmpty() || stack.pop()!=ch){
+                return false;
+            }
+        }
+        return stack.isEmpty();
+        
+    }
+}
